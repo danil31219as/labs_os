701a18704fa9cff2a8242278ff8c7878d8158642
Danil Astafurov <57491208+danil31219as@users.noreply.github.com>
2024-02-29 23:52:40 +0300

    Update task6.sh

--- lab1/task6.sh
+++ lab1/task6.sh
@@ -5,7 +5,5 @@ source_file="/var/log/anaconda/X.log"
 result_file="full.log"
 > "$result_file"
 
-grep "WARNING" "$source_file" | sed 's/^/Warning: /' >> "$result_file"
-grep "INFO" "$source_file" | sed 's/^/Information: /' >> "$result_file"
-
-cat "$result_file"
+grep "WARNING" "$source_file" | sed 's/\(WARNING\)/Warning: /' >> "$result_file"
+grep "INFO" "$source_file" | sed 's/\(INFO\)/Information: /' >> "$result_file"
