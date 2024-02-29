f32a62854c09cba8d10421f02187c1c2caa0e0c2
Danil Astafurov <57491208+danil31219as@users.noreply.github.com>
2024-03-01 00:06:24 +0300

    Update task7.sh

--- lab1/task7.sh
+++ lab1/task7.sh
@@ -3,4 +3,4 @@
 cd /etc
 > emails.lst
 
-grep -h -o -E '\b[A-Za-z0-9.]+@[A-Za-z0-9.]+\.[A-Z|a-z]{2,}\b' * >> emails.lst
+find . -type f -exec grep -h -o -E '\b[A-Za-z0-9.]+@[A-Za-z0-9.]+\.[A-Z|a-z]{2,}\b' {} + >> emails.lst
