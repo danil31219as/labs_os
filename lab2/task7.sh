df668e9a654f4b397b85194970a97e92d8389294
Danil Astafurov <57491208+danil31219as@users.noreply.github.com>
2024-03-15 14:37:10 +0300

    Update task7.sh

--- lab2/task7.sh
+++ lab2/task7.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-# Функция для получения суммарного количества считанных байт для процесса
 get_bytes_read() {
     pid=$1
-    # Используем procfs для получения информации о процессе
     read_bytes=$(grep "read_bytes" /proc/$pid/io | awk '{print $2}')
     echo $read_bytes
 }
 
-# Функция для получения строки запуска процесса
 get_command_line() {
     pid=$1
-    # Используем команду ps для получения строки запуска процесса
     command_line=$(ps -p $pid -o cmd=)
     echo $command_line
 }
 
-# Получаем начальные значения считанных байт для всех процессов
 declare -A initial_bytes
-for pid in $(pidof -x $0); do
+for pid_dir in /proc/[0-9]*; do
+    pid=$(basename "$pid_dir")
     initial_bytes[$pid]=$(get_bytes_read $pid)
 done
 
-# Ждем 1 минуту
 sleep 60
 
-# Получаем конечные значения считанных байт и вычисляем разницу
 declare -A final_bytes
-for pid in $(pidof -x $0); do
+for pid_dir in /proc/[0-9]*; do
+    pid=$(basename "$pid_dir")
     final_bytes[$pid]=$(get_bytes_read $pid)
 done
 
-# Вычисляем разницу в считанных байтах для каждого процесса
 declare -A byte_diffs
 for pid in "${!initial_bytes[@]}"; do
     initial=${initial_bytes[$pid]}
@@ -39,10 +33,8 @@ for pid in "${!initial_bytes[@]}"; do
     byte_diffs[$pid]=$((final - initial))
 done
 
-# Сортируем процессы по убыванию разницы в считанных байтах и выводим топ-3
 sorted_pids=($(for pid in "${!byte_diffs[@]}"; do echo "$pid"; done | sort -nrk2 | head -n 3))
 
-# Выводим информацию о топ-3 процессах
 for pid in "${sorted_pids[@]}"; do
     command_line=$(get_command_line $pid)
     byte_diff=${byte_diffs[$pid]}
